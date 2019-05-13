de50cc890e304e85a291325dfe3cba24da1e81d6
salvasal <salvatoresalernitano96@gmail.com>
2019-05-13 17:14:25 +0200

    creazione view nuova login

---
+++ OOSDPedaggiAutostrade/src/view/Login.java
@@ -0,0 +1,48 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblSistemaDiPedaggi = new JLabel("Sistema di Pedaggi Autostradali");
+		lblSistemaDiPedaggi.setBounds(102, 28, 209, 28);
+		contentPane.add(lblSistemaDiPedaggi);
+	}
+
+}
