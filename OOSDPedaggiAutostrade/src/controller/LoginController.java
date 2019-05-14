a7363aaa6d34f15320607bf0dfd8baa24ed7b0fc
Di Federico <Di Federico@10.171.55.63>
2019-05-14 12:17:19 +0200

    creazione login controller

--- OOSDPedaggiAutostrade/src/controller/LoginController.java
+++ OOSDPedaggiAutostrade/src/controller/LoginController.java
@@ -3,10 +3,16 @@
  */
 package controller;
 
+import model.components.GestoreUtenza;
+
 /**
- * @author
+ * @author Ludovico Di Federico Matricola 243542
  *
  */
 public class LoginController {
+	
+		public String login(String username,String password) {
+			return new GestoreUtenza().login(username, password);
+		}
 
 }
--- OOSDPedaggiAutostrade/src/model/components/GestoreUtenza.java
+++ OOSDPedaggiAutostrade/src/model/components/GestoreUtenza.java
@@ -7,7 +7,8 @@ package model.components;
  * @author Salvatore Salernitano Matricola 242016
  *
  */
-public class GestoreUtenza {
+public class GestoreUtenza implements GestoreUtenzaInterface {
+	
 	
 	
 }
