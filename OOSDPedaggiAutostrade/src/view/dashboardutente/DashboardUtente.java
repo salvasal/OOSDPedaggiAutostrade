27b9ccd74f2df79f0c09c8b1c566ea3dc1676758
lors10 <lors.93.10@gmail.com>
2019-05-15 12:19:37 +0200

    Implementazione logica logout DashboardUtente

--- OOSDPedaggiAutostrade/src/view/dashboardutente/DashboardUtente.java
+++ OOSDPedaggiAutostrade/src/view/dashboardutente/DashboardUtente.java
@@ -7,6 +7,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import view.Login;
+
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+
 public class DashboardUtente extends JFrame {
 
 	private JPanel contentPane;
@@ -34,11 +42,24 @@ public class DashboardUtente extends JFrame {
 	public DashboardUtente(String username) {
 		setTitle("Dashboard Utente");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 318);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JButton btnLogout = new JButton("Logout");
+		btnLogout.setForeground(Color.RED);
+		btnLogout.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		btnLogout.setBounds(103, 220, 255, 39);
+		contentPane.add(btnLogout);
+		btnLogout.addActionListener(new ActionListener() {
+			
+			public void actionPerformed (ActionEvent e) {
+				dispose();
+				Login l = new Login();
+				l.setVisible(true);
+			}
+		});
 	}
-
 }
