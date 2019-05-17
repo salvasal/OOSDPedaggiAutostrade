2b316f75c7c08a21df32c546ab5a473bbe044d1f
lors10 <lors.93.10@gmail.com>
2019-05-17 16:53:43 +0200

    Creazione Completa View My Tariffa

--- OOSDPedaggiAutostrade/src/view/dashboardadmin/MyTariffe.java
+++ OOSDPedaggiAutostrade/src/view/dashboardadmin/MyTariffe.java
@@ -18,7 +18,8 @@ import javax.swing.JTextField;
 public class MyTariffe extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField importoField;
+	private JTextField importoTariffeField;
+	private JTextField importoOneriField;
 
 	/**
 	 * Launch the application.
@@ -112,18 +113,43 @@ public class MyTariffe extends JFrame {
 		lblInserisciImporto.setBounds(23, 181, 230, 16);
 		panelTariffe.add(lblInserisciImporto);
 		
-		importoField = new JTextField();
-		importoField.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		importoField.setBounds(290, 176, 150, 26);
-		panelTariffe.add(importoField);
-		importoField.setColumns(10);
+		importoTariffeField = new JTextField();
+		importoTariffeField.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		importoTariffeField.setBounds(290, 176, 150, 26);
+		panelTariffe.add(importoTariffeField);
+		importoTariffeField.setColumns(10);
 		
-		JButton btnApplica = new JButton("Applica ");
-		btnApplica.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-		btnApplica.setBounds(177, 264, 117, 35);
-		panelTariffe.add(btnApplica);
+		JButton btnApplicaTariffe = new JButton("Applica ");
+		btnApplicaTariffe.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		btnApplicaTariffe.setBounds(177, 264, 117, 35);
+		panelTariffe.add(btnApplicaTariffe);
 		
 		// componenti del pannello oneri
+		
+		JLabel lblSelezionaOneri = new JLabel("Seleziona la Classe Ambientale del Veicolo");
+		lblSelezionaOneri.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblSelezionaOneri.setBounds(23, 19, 230, 16);
+		panelOneri.add(lblSelezionaOneri);
+		
+		JComboBox oneriComboBox = new JComboBox();
+		oneriComboBox.setBounds(285, 15, 155, 27);
+		panelOneri.add(oneriComboBox);
+		
+		JLabel lblImporto = new JLabel("Inserisci l'importo degli Oneri");
+		lblImporto.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblImporto.setBounds(23, 99, 230, 16);
+		panelOneri.add(lblImporto);
+		
+		importoOneriField = new JTextField();
+		importoOneriField.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		importoOneriField.setBounds(285, 95, 155, 27);
+		panelOneri.add(importoOneriField);
+		importoOneriField.setColumns(10);
+		
+		JButton btnApplicaOneri = new JButton("Applica ");
+		btnApplicaOneri.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		btnApplicaOneri.setBounds(177, 264, 117, 35);
+		panelOneri.add(btnApplicaOneri);
 		
 	}
 }
