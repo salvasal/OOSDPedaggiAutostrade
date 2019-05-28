09f885d1a4c4791b9a2f0d7d5686be1401686c88
salvasal <salvatoresalernitano96@gmail.com>
2019-05-28 15:42:01 +0200

    Modifica View AggiungiCasello

--- OOSDPedaggiAutostrade/src/view/dashboardadmin/AggiungiCasello.java
+++ OOSDPedaggiAutostrade/src/view/dashboardadmin/AggiungiCasello.java
@@ -41,7 +41,7 @@ public class AggiungiCasello extends JFrame {
 	public AggiungiCasello() {
 		setTitle("Aggiungi un Casello");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 380);
+		setBounds(100, 100, 450, 298);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -67,28 +67,19 @@ public class AggiungiCasello extends JFrame {
 		contentPane.add(nomeCaselloField);
 		nomeCaselloField.setColumns(10);
 		
-		JLabel lblCodiceAutostrada = new JLabel("Codice Autostrada");
-		lblCodiceAutostrada.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		lblCodiceAutostrada.setBounds(35, 163, 121, 16);
-		contentPane.add(lblCodiceAutostrada);
-		
-		JComboBox codiceAutostradaComboBox = new JComboBox();
-		codiceAutostradaComboBox.setBounds(189, 159, 189, 27);
-		contentPane.add(codiceAutostradaComboBox);
-		
 		JLabel lblKm = new JLabel("Km");
 		lblKm.setFont(new Font("Times New Roman", Font.PLAIN, 15));
-		lblKm.setBounds(35, 239, 61, 16);
+		lblKm.setBounds(35, 152, 61, 16);
 		contentPane.add(lblKm);
 		
 		JComboBox kmComboBox = new JComboBox();
-		kmComboBox.setBounds(189, 235, 189, 27);
+		kmComboBox.setBounds(189, 148, 189, 27);
 		contentPane.add(kmComboBox);
 		
 		JButton btnAggiungiCasello = new JButton("Aggiungi Casello");
 		btnAggiungiCasello.setForeground(Color.RED);
 		btnAggiungiCasello.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-		btnAggiungiCasello.setBounds(189, 304, 189, 35);
+		btnAggiungiCasello.setBounds(189, 208, 189, 35);
 		contentPane.add(btnAggiungiCasello);
 	}
 }
