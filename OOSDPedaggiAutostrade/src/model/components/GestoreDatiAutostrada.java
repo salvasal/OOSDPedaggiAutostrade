b8db940a2bdf2107b86f01e1568af56f893ace25
salvasal <salvatoresalernitano96@gmail.com>
2019-06-20 14:14:50 +0200

    Ristrutturazione Model Components

---
+++ OOSDPedaggiAutostrade/src/model/components/GestoreDatiAutostrada.java
@@ -0,0 +1,220 @@
+/**
+ * 
+ */
+package model.components;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+
+import model.database.Database;
+import model.interfaces.AutostradaInterface;
+
+/**
+ * @author Salernitano Salvatore Matricola 242016
+ *
+ */
+public class GestoreDatiAutostrada implements AutostradaInterface {
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#getAutostradebyUsername(java.lang.String)
+	 */
+	@Override
+	public DefaultListModel getAutostradebyUsername(String username) {
+		// TODO Auto-generated method stub
+		DefaultListModel dfm = new DefaultListModel();
+		ResultSet rs;
+		Connection cn = new Database().Connect();
+		try {
+			Statement st = cn.createStatement();
+			rs = st.executeQuery("select codice, nome, tipo, kminizio, kmfine from autostrada where amministratore='"+username+"' ");
+			while (rs.next()) {
+				dfm.addElement("Codice: "+rs.getString("codice")+" Nome: "+rs.getString("nome")+" Tipologia: "+rs.getString("tipo")+" KmInzio: "+rs.getInt("kminizio")+" KmFine: "+rs.getInt("kmfine"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dfm;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#getCasellibyIDautostrada(java.lang.String)
+	 */
+	@Override
+	public DefaultListModel getCasellibyIDautostrada(String ID) {
+		// TODO Auto-generated method stub
+		DefaultListModel dfm = new DefaultListModel();
+		ResultSet rs;
+		Connection cn = new Database().Connect();
+		try {
+			Statement st = cn.createStatement();
+			rs = st.executeQuery("select nome, km from casello where autostrada = '"+ID+"'");
+			while(rs.next()) {
+				dfm.addElement("Casello: "+rs.getString("nome")+" "+rs.getInt("km"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dfm;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#setAutostrada(java.lang.String, java.lang.String, java.lang.String, java.lang.Integer, java.lang.String)
+	 */
+	@Override
+	public void setAutostrada(String codice, String nome, String tipoSelected, Integer lunghezza, String username) {
+		// TODO Auto-generated method stub
+		Connection cn = new Database().Connect();
+		Integer kmInizio;
+		Integer kmFine;
+		Statement st;
+		Statement st2;
+		Autostrada a;
+		
+		try {
+			st = cn.createStatement();
+			ResultSet rs = st.executeQuery("select kmfine from autostrada order by kmfine DESC");
+			if(rs.next()) {
+				kmInizio = (rs.getInt("kmfine"))+1;
+				kmFine = (rs.getInt("kmfine"))+lunghezza;
+				a = new Autostrada(codice, nome, tipoSelected, kmInizio, kmFine, username);
+				st2 = cn.createStatement();
+				String tot = "insert into autostrada values('"+a.getCodice()+"','"+a.getNome()+"','"+a.getTipo()+"','"+a.getKminizio()+"','"+a.getKmfine()+"','"+a.getAmministratore()+"')";
+				st2.executeUpdate(tot);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#getkmbyID(java.lang.String)
+	 */
+	@Override
+	public ArrayList<Integer> getkmbyID(String ID) {
+		// TODO Auto-generated method stub
+		Connection con = new Database().Connect();
+		ArrayList<Integer> kmlist = new ArrayList<Integer>();
+		Integer kmInizio;
+		Integer kmFine;
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select kminizio, kmfine from autostrada where codice='"+ID+"'");
+			if(rs.next()) {
+				kmInizio = (rs.getInt("kminizio"));
+				kmFine = (rs.getInt("kmfine"));
+				for(int i=kmInizio; i<=kmFine; i++) {
+					kmlist.add(i);
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return kmlist;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#setCasello(java.lang.String, java.lang.String, java.lang.Integer, java.lang.String)
+	 */
+	@Override
+	public void setCasello(String coordinate, String nome, Integer km, String autostrada) {
+		// TODO Auto-generated method stub
+		Casello c = new Casello(coordinate, nome, km, autostrada);
+		Connection con = new Database().Connect();
+		try {
+			Statement st = con.createStatement();
+			st.executeUpdate("insert into casello values ('"+c.getCoordinate()+"','"+c.getNome()+"','"+c.getKm()+"','"+c.getAutostrada()+"')");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#getIdautostradaByusername(java.lang.String)
+	 */
+	@Override
+	public ArrayList<String> getIdautostradaByusername(String username) {
+		// TODO Auto-generated method stub
+		ArrayList<String> idautostradalist = new ArrayList<String>();
+		Connection con = new Database().Connect();
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select codice from autostrada where Amministratore='"+username+"'");
+			while(rs.next()) {
+				idautostradalist.add(rs.getString("codice"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return idautostradalist;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#deleteautostrada(java.lang.String)
+	 */
+	@Override
+	public void deleteautostrada(String codiceSelected) {
+		// TODO Auto-generated method stub
+		Connection con = new Database().Connect();
+		try {
+			Statement st = con.createStatement();
+			st.executeUpdate("delete from casello where Autostrada = '"+codiceSelected+"'");
+			Statement st2 = con.createStatement();
+			st.executeUpdate("delete from autostrada where codice ='"+codiceSelected+"'");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#getCoordinatecaselliBycodiceAutostrada(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public ArrayList<String> getCoordinatecaselliBycodiceAutostrada(String usernameAmministratore,
+			String codiceAutostrada) {
+		// TODO Auto-generated method stub
+		ArrayList<String> coordinateCaselli = new ArrayList<String>();
+		Connection con = new Database().Connect();
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select coordinate from casello inner join autostrada "
+					+ "on casello.autostrada = autostrada.codice inner join amministratore on "
+					+ "autostrada.amministratore = amministratore.username where casello.autostrada = '"+codiceAutostrada+"' ");
+			while (rs.next()) {
+				coordinateCaselli.add(rs.getString("coordinate"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return coordinateCaselli;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.interfaces.AutostradaInterface#deletecasello(java.lang.String)
+	 */
+	@Override
+	public void deletecasello(String coordinateSelected) {
+		// TODO Auto-generated method stub
+		Connection con = new Database().Connect();
+		try {
+			Statement st = con.createStatement();
+			st.executeUpdate("delete from casello where coordinate = '"+coordinateSelected+"'");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
--- OOSDPedaggiAutostrade/src/model/components/Veicolo.java
+++ OOSDPedaggiAutostrade/src/model/components/Veicolo.java
@@ -18,7 +18,7 @@ import model.interfaces.VeicoloInterface;
  * @author Salvatore Salernitano Matricola 242016
  *
  */
-public class Veicolo implements VeicoloInterface {
+public class Veicolo {
 	public String targa;
 	public String marca;
 	public String modello;
@@ -198,125 +198,5 @@ public class Veicolo implements VeicoloInterface {
 	public void setUtente(String utente) {
 		this.utente = utente;
 	}
-	/* (non-Javadoc)
-	 * @see model.interfaces.VeicoloInterface#getVeicoli(java.lang.String)
-	 */
-	@Override
-	public DefaultListModel getVeicoli(String usernameSelected) {
-		// TODO Auto-generated method stub
-		DefaultListModel dfm = new DefaultListModel();
-		ResultSet result;
-		Connection con = Database.Connect();
-		Statement st;
-		try {
-			st = con.createStatement();
-			result = st.executeQuery("select targa, marca, modello, peso, assi, altezza, anno, categoria, qtaco2, oneri from veicolo where utente='"+usernameSelected+"'");
-			while (result.next()) {
-				dfm.addElement("Targa: "+result.getString("targa")+" Marca: "+result.getString("marca")+" Modello: "+result.getString("modello")+" Assi: "+result.getInt("assi")+" Categoria: "+result.getString("categoria")+" Quantita di co2 emessa: "+result.getFloat("qtaco2")+" Oneri: "+result.getInt("oneri"));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return dfm;
-	}
-	/* (non-Javadoc)
-	 * @see model.interfaces.VeicoloInterface#getVeicolibyUsername(java.lang.String)
-	 */
-	@Override
-	public ArrayList<String> getVeicolibyUsername(String usernameutente) {
-		// TODO Auto-generated method stub
-		ArrayList<String> targalist = new ArrayList<String>();
-		Connection con = new Database().Connect();
-		try {
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("select targa from veicolo where utente ='"+usernameutente+"'");
-			while(rs.next()) {
-				targalist.add(rs.getString("targa"));
-			} 
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return targalist;
-	}
-	/* (non-Javadoc)
-	 * @see model.interfaces.VeicoloInterface#deleteVeicolo(java.lang.String)
-	 */
-	@Override
-	public void deleteVeicolo(String targa) {
-		// TODO Auto-generated method stub
-		Connection con = new Database().Connect();
-		try {
-			Statement st = con.createStatement();
-			st.executeUpdate("delete from veicolo where targa = '"+targa+"'");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	/* (non-Javadoc)
-	 * @see model.interfaces.VeicoloInterface#setVeicolo(model.components.Veicolo)
-	 */
-	@Override
-	public void setVeicolo(Veicolo v) {
-		// TODO Auto-generated method stub
-		Connection con = new Database().Connect();
-		
-		
-		switch(v.getAssi()) {
-			
-		case 2: 
-			
-			if (v.getAltezza() <= 130) {
-				v.setCategoria("A");
-			} else {v.setCategoria("B");}
-			break;
-			
-		case 3:
-			
-			v.setCategoria("3");
-			break;
-			
-		case 4:
-			
-			v.setCategoria("4");
-			break;
-			
-		case 5:
-			
-			v.setCategoria("5");
-			break;
-			
-		default: v.setCategoria("5");
-		}
-		
-		
-		try {
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("select euro, importo, valoreMin, valoreMax from oneri");
-			
-			while(rs.next()) {
-				Oneri o = new Oneri(rs.getInt("Euro"), rs.getFloat("Importo"), rs.getFloat("ValoreMin"), rs.getFloat("ValoreMax"));
-				if(o.getValoreMin() <= v.getQtaco2() && v.getQtaco2() <= o.getValoreMax()) {
-					v.setOneri(o.getEuro());
-					Statement st2 = con.createStatement();
-					st2.executeUpdate("insert into veicolo values ('"+v.getTarga()+"','"+v.getMarca()+"','"+v.getModello()+"','"+v.getPeso()+"','"+v.getAssi()+"','"+v.getAltezza()+"','"+v.getAnno()+"','"+v.getCategoria()+"','"+v.getQtaco2()+"','"+v.getOneri()+"','"+v.getUtente()+"')");
-			
-				}
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
-	
-	
-	
-	
-	
 	
 }
