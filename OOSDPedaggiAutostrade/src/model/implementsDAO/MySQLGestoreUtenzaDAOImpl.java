cf317238b9bf6db6621bead58f7ab5d3af44d1bb
salvasal <salvatoresalernitano96@gmail.com>
2019-06-22 17:18:36 +0200

    Creazione Interfaccia ed Implementazione DAO GestoreUtenza

---
+++ OOSDPedaggiAutostrade/src/model/implementsDAO/MySQLGestoreUtenzaDAOImpl.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package model.implementsDAO;
+
+import model.interfacesDAO.GestoreUtenzaDAO;
+
+/**
+ * @author Salernitano Salvatore Matricola 242016
+ *
+ */
+public class MySQLGestoreUtenzaDAOImpl implements GestoreUtenzaDAO {
+
+}
